3822384e2d9b0b8559a9d52dc90379a155cbee01
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-29 10:57:33 +0200

    CardRanks solution

---
+++ working-with-abstraction/src/CardRank/Main.java
@@ -0,0 +1,11 @@
+package CardRank;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Ranks:");
+        Arrays.stream(CardRanks.values())
+                .forEach(cardRank -> System.out.printf("Ordinal value: %d; Name value: %s%n", cardRank.ordinal(), cardRank.name()));
+    }
+}
