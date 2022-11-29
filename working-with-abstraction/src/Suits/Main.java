f67d6146a5e8ad7d2b9d303d0ceb6fe696be454d
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-29 10:55:52 +0200

    Card Suits solution

---
+++ working-with-abstraction/src/Suits/Main.java
@@ -0,0 +1,14 @@
+package Suits;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Card Suits:");
+        Arrays.stream(CardSuit.values())
+                .forEach(cardSuit -> System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.ordinal(), cardSuit.name()));
+
+    }
+}
