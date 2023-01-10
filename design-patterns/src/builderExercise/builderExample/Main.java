df0c03ddfd65c099be8abec2f9564ca17d86e844
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-10 09:33:20 +0200

    Builder pattern example

---
+++ design-patterns/src/builderExercise/builderExample/Main.java
@@ -0,0 +1,13 @@
+package builderExercise.builderExample;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = Person
+                .builder()
+                .withLastName("Petrov")
+                .withAddress("Burgas")
+                .build();
+
+    }
+}
