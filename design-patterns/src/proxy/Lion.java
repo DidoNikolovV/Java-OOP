4fb7753db2977778aa4bffcca13abb5c2626fc78
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-10 13:05:02 +0200

    Iterator and Proxy pattern examples

---
+++ design-patterns/src/proxy/Lion.java
@@ -0,0 +1,8 @@
+package proxy;
+
+public class Lion implements Animal{
+    @Override
+    public void speak() {
+        System.out.println("Roar");
+    }
+}
---
+++ design-patterns/src/proxy/LionProxy.java
@@ -0,0 +1,17 @@
+package proxy;
+
+public class LionProxy implements Animal{
+    Lion lion;
+
+    public LionProxy() {
+        this.lion = new Lion();
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Before speak method from parent");
+        lion.speak();
+        System.out.println("After speak method from parent");
+
+    }
+}
