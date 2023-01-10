33cd3cb72211b9442d86cf7b2e3caaeb542c0873
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2023-01-10 12:54:45 +0200

    Strategy Pattern example

---
+++ design-patterns/src/strategyPattern/Main.java
@@ -0,0 +1,23 @@
+package strategyPattern;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        Person person = new Person("Pesho", "Ivanov");
+        Person person2 = new Person("Ivan", "Petrov");
+        Person person3 = new Person("Dimitar", "Asenov");
+
+        people.add(person);
+        people.add(person2);
+        people.add(person3);
+
+        Collections.sort(people, new FirstNameSorter());
+        Collections.sort(people, new LastNameSorter().reversed());
+
+
+    }
+}
